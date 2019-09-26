f604faba3e0598d051eac6c9aff745c3a9678ec5
reza575 <h_moeiny@yahoo.com>
2019-09-26 10:48:42 +1000

    upload main Fragments and activities

--- app/src/main/java/com/moeiny/reza/TO_DO_Project/MainActivity.java
+++ app/src/main/java/com/moeiny/reza/TO_DO_Project/MainActivity.java
@@ -1,14 +1,81 @@
 package com.moeiny.reza.TO_DO_Project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
+import com.moeiny.reza.TO_DO_Project.fragmnts.HomeFragment;
+import com.moeiny.reza.TO_DO_Project.fragmnts.InsertFragment;
+import com.moeiny.reza.TO_DO_Project.fragmnts.MarkFragment;
+import com.moeiny.reza.TO_DO_Project.fragmnts.SerachFragment;
+import com.ss.bottomnavigation.BottomNavigation;
+import com.ss.bottomnavigation.events.OnSelectedItemChangeListener;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigation bottomNavigation;
+    FragmentTransaction fragmentTransaction;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupVews();
+    }
+
+
+    private void setupVews() {
+        bottomNavigation=(BottomNavigation)findViewById(R.id.bottom_navigation);
+        fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.rel_main_fragmentContainer,new HomeFragment());
+        fragmentTransaction.commit();
+        bottomNavigation.setOnSelectedItemChangeListener(new OnSelectedItemChangeListener() {
+            @Override
+            public void onSelectedItemChanged(int i) {
+                switch (i){
+                    case R.id.tab_home:
+                        fragmentTransaction=getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.rel_main_fragmentContainer,new HomeFragment());
+                        fragmentTransaction.commit();
+                        break;
+
+                    case R.id.tab_insert:
+                        fragmentTransaction=getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.rel_main_fragmentContainer,new InsertFragment());
+                        fragmentTransaction.commit();
+                        break;
+
+                    case R.id.tab_marked:
+                        fragmentTransaction=getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.rel_main_fragmentContainer,new MarkFragment());
+                        fragmentTransaction.commit();
+                        break;
+
+                    case R.id.tab_search:
+                        fragmentTransaction=getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.rel_main_fragmentContainer,new SerachFragment());
+                        fragmentTransaction.commit();
+                        break;
+
+
+                }
+
+            }
+
+
+
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(bottomNavigation.getSelectedItem()==0){
+            super.onBackPressed();
+        } else
+        {
+            fragmentTransaction=getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.rel_main_fragmentContainer,new HomeFragment());
+            fragmentTransaction.commit();
+        }
     }
 }
---
+++ app/src/main/java/com/moeiny/reza/TO_DO_Project/detail/DetailActivity.java
@@ -0,0 +1,85 @@
+package com.moeiny.reza.TO_DO_Project.detail;
+
+import android.content.ContentValues;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.moeiny.reza.TO_DO_Project.R;
+import com.moeiny.reza.TO_DO_Project.data.MyDatabase;
+import com.moeiny.reza.TO_DO_Project.fragmnts.HomeFragment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+public class DetailActivity extends AppCompatActivity {
+
+    MyDatabase myDatabase;
+    Integer id;
+    EditText edtTitle;
+    EditText edtDate;
+    EditText edtTime;
+    EditText edtDescription;
+    Button btnSaveChanges;
+    String mark;
+    FragmentTransaction fragmentTransaction;
+
+    String itemTitle,itemDescription,itemDate,itemTime,itemmark;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Bundle bundle=getIntent().getExtras();
+         id=bundle.getInt("items_Id");
+         itemTitle=bundle.getString("items_Title");
+         itemDescription=bundle.getString("items_Description");
+         itemDate=bundle.getString("items_Date");
+         itemTime=bundle.getString("items_Time");
+         itemmark=bundle.getString("items_Mark");
+
+
+        setView();
+    }
+
+    private void setView() {
+        myDatabase=new MyDatabase(DetailActivity.this);
+        btnSaveChanges=(Button)findViewById(R.id.btn_detailActivity_save);
+        edtTitle=(EditText) findViewById(R.id.edt_detailActivity_title);
+        edtDescription=(EditText) findViewById(R.id.edt_detailActivity_desription);
+        edtDate=(EditText) findViewById(R.id.edt_detailActivity_date);
+        edtTime=(EditText) findViewById(R.id.edt_detailActivity_time);
+        edtTitle.setText(itemTitle.toString());
+        edtDescription.setText(itemDescription.toString());
+        edtDate.setText(itemDate.toString());
+        edtTime.setText(itemTime.toString());
+
+        btnSaveChanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ((edtTitle.getText().toString()=="")|| (edtTitle.getText().toString()==null)){
+                    Toast.makeText(DetailActivity.this,"Filling title is nessecery",Toast.LENGTH_SHORT).show();
+                } else
+                {
+                    ContentValues values;
+                    values=new ContentValues();
+                    values.put("title",edtTitle.getText().toString());
+                    values.put("description",edtDescription.getText().toString());
+                    values.put("date",edtDate.getText().toString());
+                    values.put("time",edtTime.getText().toString());
+                    values.put("mark",itemmark);
+                    myDatabase.updateRow(id,values);
+                    Toast.makeText(DetailActivity.this,"Changes have been saved  ",Toast.LENGTH_SHORT).show();
+
+                    }
+                finish();
+
+                fragmentTransaction=getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.rel_main_fragmentContainer,new HomeFragment());
+                fragmentTransaction.commit();
+                }
+        });
+    }
+}
