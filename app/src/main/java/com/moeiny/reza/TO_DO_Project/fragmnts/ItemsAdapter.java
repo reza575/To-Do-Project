ea65b7707d51a677bc652ccfa001f4b94a8317c4
reza575 <h_moeiny@yahoo.com>
2019-09-27 10:49:47 +1000

    completing project

--- app/src/main/java/com/moeiny/reza/TO_DO_Project/fragmnts/ItemsAdapter.java
+++ app/src/main/java/com/moeiny/reza/TO_DO_Project/fragmnts/ItemsAdapter.java
@@ -10,16 +10,16 @@ import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.moeiny.reza.TO_DO_Project.R;
 import com.moeiny.reza.TO_DO_Project.data.MyDatabase;
 import com.moeiny.reza.TO_DO_Project.data.ToDoItems;
 import com.moeiny.reza.TO_DO_Project.detail.DetailActivity;
+
 import java.util.List;
+
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemsViewHolder> {
@@ -27,44 +27,44 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemsViewHol
     List<ToDoItems> itemsList;
     Context context;
     MyDatabase myDatabase;
-    public ItemsAdapter(Context context, List<ToDoItems> itemsList){
+    int flag;
+
+    public ItemsAdapter(Context context, List<ToDoItems> itemsList) {
         this.itemsList = itemsList;
-        this.context=context;
-        myDatabase=new MyDatabase(context);
+        this.context = context;
+        flag = 0;
+        myDatabase = new MyDatabase(context);
     }
 
-   public class ItemsViewHolder extends RecyclerView.ViewHolder {
+    public class ItemsViewHolder extends RecyclerView.ViewHolder {
         CardView parent;
-       TextView txtTitle;
-       TextView txtDescription;
-       TextView txtDate;
-       TextView txtTime;
-       CheckBox chkMark;
-       ImageView imgDeleteItems;
-       ImageView imgEditItems;
-       ImageView imgToDo;
+        TextView txtTitle;
+        TextView txtDescription;
+        TextView txtDate;
+        TextView txtTime;
+        CheckBox chkMark;
+        ImageView imgDeleteItems;
+        ImageView imgEditItems;
+        ImageView imgToDo;
 
 
-       public ItemsViewHolder(@NonNull View itemView) {
-           super(itemView);
-           txtTitle=(TextView)itemView.findViewById(R.id.txt_todorow_title);
-    //       txtDescription=(TextView)itemView.findViewById(R.id.txt_todorow_D);
-           txtDate=(TextView)itemView.findViewById(R.id.txt_todorow_date);
-           txtTime=(TextView)itemView.findViewById(R.id.txt_todorow_time);
-           chkMark=(CheckBox) itemView.findViewById(R.id.chk_todorow_mark);
-           imgDeleteItems=(ImageView)itemView.findViewById(R.id.img_todorow_delete);
-           imgEditItems=(ImageView)itemView.findViewById(R.id.img_todorow_edit);
-           imgToDo=(ImageView)itemView.findViewById(R.id.img_todorow_Icon);
-           parent=(CardView)itemView.findViewById(R.id.card_todorow_parent);
+        public ItemsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtTitle = (TextView) itemView.findViewById(R.id.txt_todorow_title);
+            txtDate = (TextView) itemView.findViewById(R.id.txt_todorow_date);
+            txtTime = (TextView) itemView.findViewById(R.id.txt_todorow_time);
+            chkMark = (CheckBox) itemView.findViewById(R.id.chk_todorow_mark);
+            imgDeleteItems = (ImageView) itemView.findViewById(R.id.img_todorow_delete);
+            imgEditItems = (ImageView) itemView.findViewById(R.id.img_todorow_edit);
+            imgToDo = (ImageView) itemView.findViewById(R.id.img_todorow_Icon);
+            parent = (CardView) itemView.findViewById(R.id.card_todorow_parent);
+        }
+    }
 
-
-       }
-   }
     @NonNull
     @Override
-
     public ItemsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.todo_row,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.todo_row, parent, false);
         return new ItemsViewHolder(view);
     }
 
@@ -73,64 +73,55 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemsViewHol
         final String description;
         final Integer id;
 
-        final ToDoItems items=itemsList.get(position);
-        final ToDoItems items2=itemsList.get(position);
+        final ToDoItems items = itemsList.get(position);
+        final ToDoItems items2 = itemsList.get(position);
         holder.txtTitle.setText(items.getTitle().toString());
         holder.txtDate.setText(items.getDate().toString());
         holder.txtTime.setText(items.getTime().toString());
-        description=items.getDescription();
-        id=items.getId();
+        description = items.getDescription();
+        id = items.getId();
         if (items.getMark().equals("1")) {
             holder.chkMark.setChecked(true);
-        }else{
+        } else {
             holder.chkMark.setChecked(false);
         }
 
         holder.chkMark.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (holder.chkMark.isChecked()){
+                if (holder.chkMark.isChecked()) {
                     ContentValues values;
-                    values=new ContentValues();
-                    values.put("mark","1");
-                    myDatabase.updateRow(id,values);
-
-                }else{
+                    values = new ContentValues();
+                    values.put("mark", "1");
+                    myDatabase.updateRow(id, values);
+                } else {
                     ContentValues values;
-                    values=new ContentValues();
-                    values.put("mark","0");
-                    myDatabase.updateRow(id,values);
-
+                    values = new ContentValues();
+                    values.put("mark", "0");
+                    myDatabase.updateRow(id, values);
                 }
-
             }
         });
-
 
         holder.imgDeleteItems.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              myDatabase.deleteRow(id);
-
-                itemsList.remove(position);
-                notifyItemRemoved(position);
-                notifyItemRangeChanged(position, itemsList.size());
-                notifyDataSetChanged();
-                }
+                myDatabase.deleteRow(id);
+            }
         });
 
         holder.imgEditItems.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(context, DetailActivity.class);
+                Intent intent = new Intent(context, DetailActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.putExtra("items_Id",items.getId());
-                intent.putExtra("items_Title",items.getTitle());
-                intent.putExtra("items_Description",items.getDescription());
-                intent.putExtra("items_Date",items.getDate());
-                intent.putExtra("items_Time",items.getTime());
-                intent.putExtra("items_Description",items.getDescription());
-                intent.putExtra("items_Mark",items.getMark());
+                intent.putExtra("items_Id", items.getId());
+                intent.putExtra("items_Title", items.getTitle());
+                intent.putExtra("items_Description", items.getDescription());
+                intent.putExtra("items_Date", items.getDate());
+                intent.putExtra("items_Time", items.getTime());
+                intent.putExtra("items_Description", items.getDescription());
+                intent.putExtra("items_Mark", items.getMark());
                 context.startActivity(intent);
             }
 
